bec2657d56ddfe0bd2917218154cc03b162df0f4
Ross Burton <ross@burtonini.com>
2021-02-13 19:20:44 +0000

    Do operations on a Layout object

--- trifold.js
+++ trifold.js
@@ -1,49 +1,50 @@
 'use strict';
 
-const margin = 10;
-const pageWidth = 297;
-const pageHeight = 210;
-const thirds = pageWidth / 3;
-
-let widePanel = thirds+1;
-let narrowPanel = pageWidth - widePanel * 2;
-
-function getOutsideMetrics() {
-  return {
-    pageWidth: pageWidth,
-    pageHeight: pageHeight,
-    margin: margin,
-    folds: [narrowPanel, narrowPanel + widePanel],
-    panels: [
-      { x: margin, y: margin, w: narrowPanel - margin * 2, h: pageHeight - margin * 2 },
-      { x: narrowPanel + margin, y: margin, w: widePanel - margin * 2, h: pageHeight - margin * 2 },
-      { x: narrowPanel + widePanel + margin, y: margin, w: widePanel - margin * 2, h: pageHeight - margin * 2 }
-   ]
-  };
+function Layout() {
+    this.pageWidth = undefined;
+    this.pageHeight = undefined;
+    this.margin = undefined;
+    this.folds = null;
+    this.panels = null;
 }
 
-function getInsideMetrics() {
-  return {
-    pageWidth: pageWidth,
-    pageHeight: pageHeight,
-    margin: margin,
-    folds: [widePanel, widePanel + widePanel],
-    panels: [
-      { x: margin, y: margin, w: widePanel-margin*1.5, h: pageHeight - margin * 2 },
-      { x: widePanel + margin / 2, y: margin, w: widePanel-margin, h: pageHeight - margin * 2 },
-      { x: widePanel * 2 + margin / 2, y: margin, w: narrowPanel-margin*1.5, h: pageHeight - margin * 2 }
-   ]
-  };
+function getOutsideMetrics(layout) {
+    const thirds = layout.pageWidth / 3;
+    const widePanel = thirds + 1;
+    const narrowPanel = layout.pageWidth - widePanel * 2;
+
+    /* TODO: Can likely generalise this logic further to just iterate over the folds */
+    layout.folds = [narrowPanel, narrowPanel + widePanel];
+    layout.panels = [
+      { x: layout.margin, y: layout.margin, w: narrowPanel - layout.margin * 2, h: layout.pageHeight - layout.margin * 2 },
+      { x: narrowPanel + layout.margin, y: layout.margin, w: widePanel - layout.margin * 2, h: layout.pageHeight - layout.margin * 2 },
+      { x: narrowPanel + widePanel + layout.margin, y: layout.margin, w: widePanel - layout.margin * 2, h: layout.pageHeight - layout.margin * 2 }
+    ];
+    return layout;
 }
 
-function drawMetrics(metrics) {
-  let draw = SVG().addTo('body').size(2*pageWidth, 2*pageHeight).viewbox(0, 0, pageWidth + 5 , pageHeight + 5)
+function getInsideMetrics(layout) {
+    const thirds = layout.pageWidth / 3;
+    const widePanel = thirds + 1;
+    const narrowPanel = layout.pageWidth - widePanel * 2;
+
+    layout.folds = [widePanel, widePanel + widePanel];
+    layout.panels = [
+      { x: layout.margin, y: layout.margin, w: widePanel-layout.margin*1.5, h: layout.pageHeight - layout.margin * 2 },
+      { x: widePanel + layout.margin / 2, y: layout.margin, w: widePanel-layout.margin, h: layout.pageHeight - layout.margin * 2 },
+      { x: widePanel * 2 + layout.margin / 2, y: layout.margin, w: narrowPanel-layout.margin*1.5, h: layout.pageHeight - layout.margin * 2 }
+    ];
+    return layout;
+}
+
+function drawMetrics(layout) {
+  let draw = SVG().addTo('body').size(2*layout.pageWidth, 2*layout.pageHeight).viewbox(0, 0, layout.pageWidth + 5 , layout.pageHeight + 5)
 
   /* TODO Use CSS. Dotted grey for folds, solid grey for margins, drop shadow on outline */
-  for (let fold of metrics.folds)
-    draw.line(fold, 0, fold, metrics.pageHeight).stroke('#aaa');
-  for (let p of metrics.panels)
+  for (let fold of layout.folds)
+    draw.line(fold, 0, fold, layout.pageHeight).stroke('#aaa');
+  for (let p of layout.panels)
     draw.rect(p.w, p.h).move(p.x, p.y).fill('transparent').stroke('#aaa');
   /* TODO off-by-one with the origin again. transform to offset everything 5px in for the shadow? */
-  draw.rect(metrics.pageWidth, metrics.pageHeight).move(0, 0).fill('transparent').stroke('#000');
+  draw.rect(layout.pageWidth, layout.pageHeight).move(0, 0).fill('transparent').stroke('#000');
 }
